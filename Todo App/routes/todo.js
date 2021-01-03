f78ebb849a45c09d1160e8f7334a496d5a8e133e
AnshulPandey-01 <anshulpandey.indore@gmail.com>
2021-01-04 01:43:37 +0530

    Edit Functionality

--- Todo App/routes/todo.js
+++ Todo App/routes/todo.js
@@ -3,13 +3,20 @@ const Todo = require("../models/Todo");
 
 router.post("/add/todo", (req, res) => {
     const {todo} = req.body;
-    const newTodo = new Todo({todo})
+    const newTodo = new Todo({todo});
 
     newTodo.save().then(() => res.redirect("/")).catch((err) => console.log(err));
     
-}).get("/delete/todo/:_id", (req, res) =>{
+}).delete("/delete/todo/:_id", async(req, res) =>{
     const {_id} = req.params;
-    Todo.deleteOne({_id}).then(() => res.redirect("/")).catch((err) => console.log(err));
-})
+    await Todo.deleteOne({_id}).then(() => res.redirect("/")).catch((err) => console.log(err));
+
+}).put("/edit/todo/:id", async(req, res) =>{
+    const {id} = req.params;
+    let updatedTodo = {};
+    if(req.body.todo) updatedTodo.todo = req.body.todo;
+    updatedTodo = {$set: updatedTodo};
+    await Todo.updateOne({_id: id}, updatedTodo).then(() => res.redirect("/")).catch((err) => console.log(err));
+});
 
 module.exports = router;
