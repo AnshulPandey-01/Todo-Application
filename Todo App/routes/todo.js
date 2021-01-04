20063967950915960e974a8267fd4a6e4cea4ace
AnshulPandey-01 <anshulpandey.indore@gmail.com>
2021-01-05 01:02:22 +0530

    fixed delete

--- Todo App/routes/todo.js
+++ Todo App/routes/todo.js
@@ -9,14 +9,14 @@ router.post("/add/todo", (req, res) => {
     
 }).delete("/delete/todo/:_id", async(req, res) =>{
     const {_id} = req.params;
-    await Todo.deleteOne({_id}).then(() => res.redirect("/")).catch((err) => console.log(err));
+    await Todo.deleteOne({_id}).then(() => res.json({redirect: "/"})).catch((err) => console.log(err));
 
-}).put("/edit/todo/:id", async(req, res) =>{
-    const {id} = req.params;
+}).put("/edit/todo/:_id", async(req, res) =>{
+    const {_id} = req.params;
     let updatedTodo = {};
     if(req.body.todo) updatedTodo.todo = req.body.todo;
     updatedTodo = {$set: updatedTodo};
-    await Todo.updateOne({_id: id}, updatedTodo).then(() => res.redirect("/")).catch((err) => console.log(err));
+    await Todo.updateOne({_id}, updatedTodo).then(() => res.redirect("/")).catch((err) => console.log(err));
 });
 
 module.exports = router;
